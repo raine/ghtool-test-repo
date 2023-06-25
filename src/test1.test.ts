6a05f66a040a78b86fae642016a6dcb378fce994
Raine Virta <raine.virta@gmail.com>
2023-06-25 12:16:32 +0300

    init

---
+++ src/test1.test.ts
@@ -0,0 +1,9 @@
+describe("test1", () => {
+  it("succeeds", () => {
+    expect(true).toBe(true);
+  });
+
+  it("fails", () => {
+    expect(true).toBe(false);
+  });
+});
---
+++ src/test2.test.ts
@@ -0,0 +1,9 @@
+describe("test2", () => {
+  it("succeeds", () => {
+    expect(true).toBe(true);
+  });
+
+  it("fails", () => {
+    expect(true).toBe(false);
+  });
+});
